4770532d40ff83e5b0dcc0b99556e373c9dd16b8
Ibrahima CAMARA <icamara@wynd.eu>
2016-11-27 15:50:29 +0100

    Initial commit

---
+++ prism.mvvm/BootStrapper.cs
@@ -0,0 +1,32 @@
+﻿using prism.mvvm.Views;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace prism.mvvm
+{
+    public class BootStrapper:UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            //return Container.Resolve(;
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterType(typeof(object), typeof(viewA), "viewA", null);
+            Container.RegisterType(typeof(object), typeof(viewA), "viewB", null);
+        }
+    }
+}
---
+++ prism.mvvm/ViewModels/viewAViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace prism.mvvm.ViewModels
+{
+    public class viewAViewModel:BindableBase
+    {
+        private string _firstname="Ibrahima Sory";
+        public string FirstName
+        {
+            get { return _firstname; }
+            set { SetProperty(ref _firstname, value); }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set { SetProperty(ref _lastName, value); }
+        }
+
+        private DateTime? _lastUpdated;
+        public DateTime? LastUpdated
+        {
+            get { return _lastUpdated; }
+            set { SetProperty(ref _lastUpdated, value); }
+        }
+
+        public DelegateCommand UpdateCommand { get; set; }
+
+        public viewAViewModel()
+        {
+            UpdateCommand = new DelegateCommand(execute, canExecute).ObservesProperty(()=>FirstName).ObservesProperty(()=>LastName);
+        }
+
+        private bool canExecute()
+        {
+            return !String.IsNullOrWhiteSpace(FirstName) & !String.IsNullOrWhiteSpace(LastName);
+        }
+
+        private void execute()
+        {
+            LastUpdated = DateTime.Now;
+        }
+    }
+}
